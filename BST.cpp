82f7cd222030247c85f65dc21bfc9567fc7ac6b5
Pink-DeF <toni_tarasov_06@mail.ru>
2025-05-01 16:08:49 +0300

    First version
    Add .h und .cpp

---
+++ BST.cpp
@@ -0,0 +1,245 @@
+#include "BST.h"
+
+//Node
+BinarySearchTree::Node::Node(Key key, Value value,
+    Node* parent, Node* left, Node* right)
+    : parent(parent), left(left), right(right)
+{
+    if (!parent) { color = BLACK; }
+    keyValuePair = std::make_pair (key, value);
+}
+
+BinarySearchTree::Node::Node(const Node& other)
+{
+    if (*this == other) { return; }
+    parent = other.parent;
+    left = other.left;
+    right = other.right;
+
+    keyValuePair = other.keyValuePair;
+}
+
+bool BinarySearchTree::Node::operator==(const Node& other) const
+{
+    if (keyValuePair.first == other.keyValuePair.first) { return 1; }
+    return 0;
+}
+
+void BinarySearchTree::Node::output_node_tree() const
+{
+    if (this == nullptr)
+    {
+        std::cout << this;
+        return;
+    }
+    std::cout << "      " << keyValuePair.first << "        " << std::endl;
+    if (left)
+    {
+        std::cout << left->keyValuePair.first << "                ";
+    }
+    else
+    {
+        std::cout << "null" << "                ";
+    }
+    if (right)
+    {
+        std::cout << right->keyValuePair.first << "                " << std::endl;
+    }
+    else
+    {
+        std::cout << "null" << "                ";
+    }
+    std::cout << std::endl;
+}
+
+BinarySearchTree::Node* BinarySearchTree::Node::find(Key key)
+{
+    Node* temp = this;
+    while (temp->keyValuePair.first != key)
+    {
+        if (key > temp->keyValuePair.first)
+        {
+            temp = temp->right;
+        }
+        else
+        {
+            temp = temp->left;
+        }
+    }
+    return temp;
+}
+BinarySearchTree::Node* BinarySearchTree::Node::find_min()
+{
+    Node* temp = this;
+    while (temp->left != nullptr)
+    {
+        temp = temp->left;
+    }
+    return temp;
+}
+BinarySearchTree::Node* BinarySearchTree::Node::find_max()
+{
+    Node* temp = this;
+    while (temp->right != nullptr)
+    {
+        temp = temp->right;
+    }
+    return temp;
+}
+
+void BinarySearchTree::Node::insert(const Key& key, const Value& value)
+{
+    Node location = *this;
+    std::pair<Node*, bool> direction;
+
+    if (key < location.keyValuePair.first) { direction = std::make_pair(left, 1); }
+    else { direction = std::make_pair(right, 0); }
+
+    while (direction.first != nullptr)
+    {
+        location = *direction.first;
+
+        if (key < location.keyValuePair.first) { direction = std::make_pair(left, 1); }
+        else { direction = std::make_pair(right, 0); }
+    }
+
+    if (direction.second)
+    {
+        location.left = new Node(key, value, &location, nullptr, nullptr);
+    }
+    else
+    {
+        location.right = new Node(key, value, &location, nullptr, nullptr);
+    }
+
+    return;
+}
+void BinarySearchTree::Node::erase(const Key& key)
+{
+    Node* find_node = this->find(key);
+    Node* parent_find_node = find_node->parent;
+    Node* temp;
+    Node* temp2;
+    if (find_node == nullptr)
+    {
+        return;
+    }
+    //удаление узла, у которого вообще нет наследников
+    if ((find_node->left == nullptr) && (find_node->right == nullptr))
+    {
+        if (key >= parent_find_node->keyValuePair.first)
+        {
+            parent_find_node->right = nullptr;
+        }
+        else
+        {
+            parent_find_node->left = nullptr;
+        }
+    }
+    //удаление узла, у которого есть 1 наследник
+    else if ((find_node->left == nullptr) + (find_node->right == nullptr) == 1)
+    {
+        if (find_node->right != nullptr)
+        {
+            temp = find_node->right;
+        }
+        else
+        {
+            temp = find_node->left;
+        }
+        if (key >= parent_find_node->keyValuePair.first)
+        {
+            parent_find_node->right = temp;
+        }
+        else
+        {
+            parent_find_node->left = temp;
+        }
+    }
+    //удаление узла, у которого 2 наследника
+    else
+    {
+        temp = find_node->right; //правая часть удаляемого узла
+        temp2 = find_node->left; //левая часть удаляемого узла
+        Node* temp_min = temp->find_min();
+        temp_min->left = temp2;
+        if (key >= parent_find_node->keyValuePair.first)
+        {
+            parent_find_node->right = temp;
+        }
+        else
+        {
+            parent_find_node->left = temp;
+        }
+    }
+    delete find_node;
+}
+
+
+//Rebalancing
+void BinarySearchTree::leftrotate(Node* node)
+{
+
+}
+void BinarySearchTree::rightrotate(Node* node)
+{
+
+}
+void BinarySearchTree::rebalancing(Node* node)
+{
+    Node* uncle;
+    while (node->parent->color == RED)
+    {
+        if (node->parent == node->parent->parent->left)
+        {
+            uncle = node->parent->parent->right;
+            if (uncle)
+            {
+                if (uncle->color == RED)
+                {
+                    node->parent->color = BLACK;
+                    uncle->color = BLACK;
+                    node->parent->parent->color = RED;
+                    node = node->parent->parent;
+                }
+            }
+            else
+            {
+                if (node == node->parent->right)
+                {
+                    node = node->parent;
+                    leftrotate(node);
+                }
+                node->parent->color = BLACK;
+                node->parent->parent->color = RED;
+                rightrotate(node);
+            }
+        }
+        else
+        {
+            uncle = node->parent->parent->left;
+            if (uncle)
+            {
+                if (uncle->color == RED)
+                {
+                    node->parent->color = BLACK;
+                    uncle->color = BLACK;
+                    node->parent->parent->color = RED;
+                    node = node->parent->parent;
+                }
+            }
+            else
+            {
+                if (node == node->parent->left)
+                {
+                    node = node->parent;
+                    rightrotate(node);
+                }
+                node->parent->color = BLACK;
+                node->parent->parent->color = RED;
+                leftrotate(node);
+            }
+        }
+    }
+
+}
---
+++ BST.h
@@ -0,0 +1,158 @@
+#pragma once
+#include <utility>
+#include <cstdint>
+#include <string>
+#include <iostream>
+
+using Key = uint32_t;
+using Value = double;
+
+class BinarySearchTree 
+{
+    enum color
+    {
+        BLACK = 0,
+        RED = 1
+    };
+    struct Node 
+    {
+    public:
+        Node() = default;
+        Node(Key key, Value value, 
+             Node* parent = nullptr, Node* left = nullptr, 
+             Node* right = nullptr);
+        Node(const Node &other);
+        bool operator==(const Node &other) const;
+
+        void output_node_tree() const;
+
+        Node* find(Key key);
+        Node* find_min();
+        Node* find_max();
+
+        void insert(const Key &key, const Value &value);
+        void erase(const Key &key);
+
+        std::pair<Key, Value> keyValuePair; 
+        bool color = RED;
+        Node* parent = nullptr;
+        Node* left = nullptr;   
+        Node* right = nullptr;  
+    };
+    void leftrotate(Node* node);
+    void rightrotate(Node* node);
+    void rebalancing(Node* node);
+
+public:
+    //! Конструктор по умолчанию
+    BinarySearchTree() = default;
+    //! Конструктор копирования
+    explicit BinarySearchTree(const BinarySearchTree &other);
+    //! Оператор присваивания копированием
+    BinarySearchTree &operator=(const BinarySearchTree &other);
+    //! Конструктор перемещения
+    explicit BinarySearchTree(BinarySearchTree &&other) noexcept;
+    //! Оператор присваивания перемещением
+    BinarySearchTree &operator=(BinarySearchTree &&other) noexcept;
+    //! Деструктор
+    ~BinarySearchTree();
+
+    //! \brief Итератор бинарного дерева поиска
+    //! \note Обходит дерево последовательно от узла с меньшим ключом к узлу с большим 
+    class Iterator 
+    {
+    public:
+        explicit Iterator(Node *node);
+
+        std::pair<Key, Value> &operator*();
+        const std::pair<Key, Value> &operator*() const;
+
+        std::pair<Key, Value> *operator->();
+        const std::pair<Key, Value> *operator->() const;
+
+        Iterator operator++();
+        Iterator operator++(int);
+
+        Iterator operator--();
+        Iterator operator--(int);
+
+        bool operator==(const Iterator &other) const;
+        bool operator!=(const Iterator &other) const;
+
+    private:
+        Node *_node;
+    };
+
+    //! Константный итератор бинарного дерева поиска
+    class ConstIterator 
+    {
+    public:
+        explicit ConstIterator(const Node *node);
+
+        const std::pair<Key, Value> &operator*() const;
+        const std::pair<Key, Value> *operator->() const;
+
+        ConstIterator operator++();
+        ConstIterator operator++(int);
+
+        ConstIterator operator--();
+        ConstIterator operator--(int);
+
+        bool operator==(const ConstIterator &other) const;
+        bool operator!=(const ConstIterator &other) const;
+
+    private:
+        const Node *_node;
+    };
+
+    //! Вставить элемент с ключем key и значением value
+    void insert(const Key &key, const Value &value);
+    //! Удалить все элементы с ключем key
+    void erase(const Key &key);
+    //! Найти первый элемент в дереве, равный ключу key
+    ConstIterator find(const Key &key) const;
+    //! Найти первый элемент в дереве, равный ключу key
+    Iterator find(const Key &key);
+
+    /*!***********************************************************
+    Найти все элементы, у которых ключ равен key:
+      - первый итератор пары - первый элемент в дереве, равный key
+      - второй итератор пары - первый элемент в дереве больший, чем key
+     
+    [pair.first, pair.second) - полуинтервал, содержащий все 
+    элементы с ключем key
+    **************************************************************/
+    std::pair<Iterator, Iterator> equalRange(const Key &key);
+    std::pair<ConstIterator, ConstIterator> equalRange(const Key &key) const;
+    
+    //! Получить итератор на элемент с наименьшим ключем в дереве
+    ConstIterator min() const;
+    //! Получить итератор на элемент с наибольшим ключем в дереве
+    ConstIterator max() const;
+    //! Получить итератор на элемент с ключем key с наименьшим значением 
+    ConstIterator min(const Key &key) const;
+    //! Получить итератор на элемент с ключем key с наибольшим значением
+    ConstIterator max(const Key &key) const;
+
+    //! Получить итератор на первый элемент дерева (элемент с наименьшим key)
+    Iterator begin();
+    //! Получить итератор на элемент, следующий за последним элементом дерева
+    //! \note Т.е. tree.end()-- == tree.max()
+    Iterator end();
+
+    //! Получить константный итератор на начало
+    ConstIterator cbegin() const;
+    //! Получить константный итератор на конец
+    ConstIterator cend() const;
+
+    //! Получить размер дерева
+    size_t size() const;
+    //! Вывести дерево в консоль
+    void output_tree();
+	//! Получить максимальную высоту в дереве
+	size_t max_height() const;
+
+private:
+    size_t _size = 0; //!< размер дерева
+    Node *_root = nullptr; //!< корневой узел дерева
+};
